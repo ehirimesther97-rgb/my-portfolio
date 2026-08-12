5088a666fc9d7e2f704a5ccc0939f757eff8439c
ehirimesther97-rgb <ehirimesther97@gmail.com>
2026-08-12 22:14:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function welcomeMessage() {
-    alert("Welcome to my portfolio! 😊");
-}
+// Welcome message
+console.log("Welcome to my portfolio!");
+
+// Update the copyright year automatically
+document.getElementById("year").textContent = new Date().getFullYear();
